63fce4466bb175bd9cab96a5ff3080e2a4552ff6
Ashish Deuja <86704158+Ashishdeuja@users.noreply.github.com>
2022-07-19 20:22:20 +0545

    Allowed Host

--- myshop/settings.py
+++ myshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'u47=yma1h6sf=ertk=*%ga^_(1x_d&n571cn%qx#c0sq_&_6ww'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myshop-web.herokuapp.com']
 
 
 # Application definition
